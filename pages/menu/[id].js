28073b8d34157b5ef7f1187deef710d712addd46
Hamidevity <127018991+Hamidevity@users.noreply.github.com>
2023-06-14 12:50:19 -0700

    Update [id].js

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -16,7 +16,7 @@ function Details({data}) {
 
 export default Details
 export async function getStaticPaths(){
-    const res = await fetch(`${process.env.BASE_URL}`);
+    const res = await fetch(`${process.env.BASE_URL}/data`);
     const json = await res.json()
     const data = json.slice(0 , 10);
     const paths = data.map((food) => ({
@@ -33,7 +33,7 @@ export async function getStaticPaths(){
 
 export async function getStaticProps(context){
     const {params: {id} } = context
-    const res =await fetch (`${process.env.BASE_URL}/${id}`);
+    const res =await fetch (`${process.env.BASE_URL}/data/${id}`);
     const data = await res.json();
 
 
